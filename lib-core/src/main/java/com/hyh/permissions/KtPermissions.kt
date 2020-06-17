2fca4c0951daa63ebca4b22355ee74148a3c7d64
EricHyh <252882326@qq.com>
2020-06-17 17:47:39 +0800

    ...

---
+++ lib-core/src/main/java/com/hyh/permissions/KtPermissions.kt
@@ -0,0 +1,9 @@
+package com.hyh.permissions
+
+/**
+ * @description
+ *
+ * @author Administrator
+ * @data  2020/6/16
+ */
+class KtPermissions
---
+++ lib-core/src/main/java/com/hyh/permissions/PermissionsRq.kt
@@ -0,0 +1,10 @@
+package com.hyh.permissions
+
+/**
+ * @description
+ *
+ * @author Administrator
+ * @data  2020/6/16
+ */
+class PermissionsRq {
+}
